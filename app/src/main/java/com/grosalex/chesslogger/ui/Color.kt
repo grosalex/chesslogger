6e120d3a0816375d5529200ba6deede35b8c49b3
Alex Bruneau <alexandre.bruneau@glose.com>
2020-12-11 07:21:21 +0100

    initial commit

---
+++ app/src/main/java/com/grosalex/chesslogger/ui/Color.kt
@@ -0,0 +1,14 @@
+package com.grosalex.chesslogger.ui
+
+import androidx.compose.ui.graphics.Color
+
+val primary  = Color(0xFF0091ea)
+val primaryLight = Color(0xFF64c1ff)
+val primaryDark = Color(0xFF0064b7)
+
+val secondary = Color(0xFF00e575)
+val secondaryLight = Color(0xFF66ffa5)
+val secondaryDark = Color(0xFF00b147)
+
+val textOnPrimary = Color(0xFF000000)
+val textOnSecondary = Color(0xFF000000)
---
+++ app/src/main/java/com/grosalex/chesslogger/ui/Theme.kt
@@ -0,0 +1,37 @@
+package com.grosalex.chesslogger.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+val lightColors = lightColors(
+    primary = primary,
+    primaryVariant = primaryLight,
+    secondary = secondary,
+    secondaryVariant = secondaryLight,
+    onPrimary = textOnPrimary,
+    onSecondary = textOnSecondary
+    /* Other default colors to override
+  background = Color.White,
+  surface = Color.White,
+  onBackground = Color.Black,
+  onSurface = Color.Black,
+  */
+)
+@Composable
+fun ChessLoggerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    /*val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }*/
+
+    MaterialTheme(
+            colors = lightColors,
+            typography = typography,
+            shapes = shapes,
+            content = content
+    )
+}
