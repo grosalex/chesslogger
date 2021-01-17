d4dfa3082932e7f5c5c98b5e565940535656eae9
Alex Bruneau <alexandre.bruneau@glose.com>
2021-01-17 11:41:20 +0100

    add white border when on dark mode

--- app/src/main/java/com/grosalex/chesslogger/components/PastMovements.kt
+++ app/src/main/java/com/grosalex/chesslogger/components/PastMovements.kt
@@ -3,16 +3,15 @@ package com.grosalex.chesslogger.components
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.grosalex.chesslogger.models.Key
-import com.grosalex.chesslogger.ui.black
 
 @Composable
 fun MovementsList(movements: List<Pair<List<Key>?, List<Key>?>>, currentMove: List<Key>) {
@@ -47,14 +46,19 @@ fun PairedMovement(
 }
 
 @Composable
-fun Movement(movement: String?, currentMove: List<Key>?) =
+fun Movement(movement: String?, currentMove: List<Key>?) {
     Box(
-        modifier = Modifier.border(1.dp, black).padding(4.dp)
+        modifier = Modifier
+            .border(1.dp, MaterialTheme.colors.onSurface)
+            .padding(4.dp)
     ) {
         Text(
-            modifier = Modifier.width(64.dp).padding(8.dp),
+            modifier = Modifier
+                .width(64.dp)
+                .padding(8.dp),
             text = movement ?: currentMove?.map { stringResource(id = it.notationStringRes) }
                 ?.joinToString(separator = "") ?: " - ",
             textAlign = TextAlign.Center
         )
     }
+}
